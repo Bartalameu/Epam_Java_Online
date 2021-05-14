f9a321dac62921a8497c8f3356d0b5aee3793426
bartalameu <bartalameu@gmail.com>
2021-05-14 14:20:54 +0200

    Add sixth case

---
+++ src/Linear_programms/sixth/Point.java
@@ -0,0 +1,10 @@
+package Linear_programms.sixth;
+
+public class Point {
+    int x,y;
+    public Point ( int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+
+}
---
+++ src/Linear_programms/sixth/Space_checker.java
@@ -0,0 +1,9 @@
+package Linear_programms.sixth;
+
+import java.util.ArrayList;
+
+public class Space_checker {
+    public static void main(String[] args) {
+        ArrayList <String>  figure =new ArrayList<>();
+    }
+}
