276465c9bfc8f97f7a3e9cc5e8c1870fe661591f
bartalameu <bartalameu@gmail.com>
2021-08-04 23:30:29 +0200

    Add sorting - task 5

---
+++ src/com/github/bartalameu/epam_java_online/algorithmization/sorting/Fifth.java
@@ -0,0 +1,11 @@
+package com.github.bartalameu.epam_java_online.algorithmization.sorting;
+
+public class Fifth {
+    public static void main(String[] args) {
+
+    }
+
+    static void duble_search(int[] arr) {
+
+    }
+}
