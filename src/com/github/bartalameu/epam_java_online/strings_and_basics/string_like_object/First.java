04748f607140865d27b4e29d7dc222380ecbe07d
bartalameu <bartalameu@gmail.com>
2021-09-12 18:29:55 +0200

    Add strings_and_basics.string_like_object - task 1

---
+++ src/com/github/bartalameu/epam_java_online/strings_and_basics/string_like_object/First.java
@@ -0,0 +1,28 @@
+package com.github.bartalameu.epam_java_online.strings_and_basics.string_like_object;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class First {
+    public static void main(String[] args) {
+        String sp = " Let`s      Find the         biggest  void     ";
+        System.out.println(sp);
+        //For illustrative purposes replace the space to count
+        int[] max = new int[sp.length()];
+        int count = 0;
+        StringBuilder b = new StringBuilder(sp);
+        for (int i = 0; i < b.length(); i++) {
+            if (b.substring(i, i + 1).equals(" ")) {
+                b.replace(i, i + 1, Integer.toString(count + 1));
+                count++;
+            } else if (!b.substring(i, i + 1).equals(" ")) {
+                max[i] = count;
+                count = 0;
+            }
+
+        }
+        System.out.println("biggest space is = " + Arrays.stream(max).max().getAsInt());
+
+        System.out.println(b);
+    }
+}
